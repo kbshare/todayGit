50ce1d48ed61cf32bcd6cf5013847e95ca141c7f
YYheroking <370484600@qq.com>
2016-09-02 14:10:46 +0800

    2创建KBNav和KBTabbar控制器

--- KBGitTest/KBGitTest/AppDelegate.m
+++ KBGitTest/KBGitTest/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "KBTabBarController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +16,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    
+    self.window = [[UIWindow alloc]init];
+    self.window.rootViewController = [[KBTabBarController alloc]init];
+    
+    [self.window makeKeyAndVisible];
+    
+    
     return YES;
 }
 
---
+++ KBGitTest/KBGitTest/KBNavViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBNavViewController.h
+//  KBGitTest
+//
+//  Created by yaoyun on 16/9/2.
+//  Copyright © 2016年 yaoyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBNavViewController : UINavigationController
+
+@end
